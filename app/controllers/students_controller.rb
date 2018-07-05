0783358b93e526fb8855fe3c690e79e7a6ce283d
Alex Zhang <alexgzhang92@gmail.com>
2018-07-05 20:36:41 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,4 +10,10 @@ class StudentsController < ApplicationController
     @school_class = SchoolClass.create(post_params(:title, :description))
     redirect_to school_class_path(@school_class)
   end
+  
+  private
+  
+  def post_params(*args)
+    params.require(:post).permit(*args)
+  end
 end
